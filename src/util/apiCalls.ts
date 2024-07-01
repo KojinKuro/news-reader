fc338d27eed73f8c67b567b725fc34678123f121
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-06-30 20:24:33 -0700

    Update to use API

--- src/util/apiCalls.ts
+++ src/util/apiCalls.ts
@@ -1,20 +1,20 @@
-import { mockArticles } from "../data/mockArticles";
+// import { mockArticles } from "../data/mockArticles";
 import { APIResponse } from "../types/type";
 
 export function getNews(page: number = 1): Promise<APIResponse> {
-  // const query = encodeURIComponent("videogame OR video game");
-  // const apiKey = import.meta.env.VITE_API_KEY;
-  // return fetch(
-  //   `https://newsapi.org/v2/everything?q=${query}&page=${page}&sortBy=publishedAt&apiKey=${apiKey}`
-  // ).then((res) => {
-  //   if (!res.ok) {
-  //     throw Error("failed to get news");
-  //   } else {
-  //     return res.json();
-  //   }
-  // });
-
-  return new Promise((resolve) => {
-    resolve(mockArticles);
+  const query = encodeURIComponent('videogame OR "video game"');
+  const apiKey = import.meta.env.VITE_API_KEY;
+  return fetch(
+    `https://newsapi.org/v2/everything?q=${query}&page=${page}&sortBy=publishedAt&apiKey=${apiKey}`
+  ).then((res) => {
+    if (!res.ok) {
+      throw Error("failed to get news");
+    } else {
+      return res.json();
+    }
   });
+
+  // return new Promise((resolve) => {
+  //   resolve(mockArticles);
+  // });
 }
